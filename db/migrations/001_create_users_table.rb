c870e4699f39260984aec82b70141e38fc63d5e0
rnparks <rnparks@gmail.com>
2014-09-24 14:07:46 -0400

    adds_user table is successfully created

--- db/migrations/001_create_users_table.rb
+++ db/migrations/001_create_users_table.rb
@@ -3,12 +3,12 @@ Sequel.migration do
     create_table(:users) do
       primary_key :id
       String :email, :size=>255
-      String :name, size =>64
-      String :gener, size => 8
-      String :picture, size => 255
+      String :name, :size =>64
+      String :gener, :size => 8
+      String :picture, :size => 255
       Integer :dob
-      String :location, size => 255
-      String :password, size => 64
+      String :location, :size => 255
+      String :password, :size => 64
     end
   end
 end
